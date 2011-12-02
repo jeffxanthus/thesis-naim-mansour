0a31b4e95c336de9103157b878410370655d39bb
naimdegrote@gmail.com <naimdegrote@gmail.com@a6849883-d5ac-729f-ebde-f3054216de5f>
2011-12-02 17:45:57 +0000

    deletion after mess

--- trunk/MATLAB/BarkToBW.m
+++
@@ -1,6 +0,0 @@
-function [ freq ] = BarkToBW( b )
-%BarkToFreq converts from bark to bandwidth (Hz)
-
-freq = 52548/(b^2-52.56*b+690.39);
-end
-
--- trunk/MATLAB/NESTA_v1.1/RecPF_v1.1/utilities/dctPhi.m
+++
@@ -1,6 +0,0 @@
-function Y = dctPhi(X,trans)
-if ~trans;
-    Y = dct2(X);
-else
-    Y = idct2(X);
-end
