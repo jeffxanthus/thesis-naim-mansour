194cf906caabcda8cb483a85a4dbc424a8ea1f25
naimdegrote <naimdegrote@gmail.com>
2011-12-02 17:51:30 +0000

    initial commit after crash

---
+++ MATLAB/CSDeclip.m
@@ -0,0 +1,123 @@
+function [x,r] = CSDeclip(data)
+%CSDECLIP - data is already clipped signal
+%Naim Mansour
+global methodChoice
+% global A
+% global samples
+global regularization
+
+addpath('NESTA_v1.1')
+
+[rs cs]=size(data);
+if(rs~=1)
+    data=data';
+end
+[rs2 cs2]=size(data);
+N=cs2;
+
+M=[];
+Mn=[];
+Mp=[];
+samples=[];
+MaxS=max(data);
+MinS=min(data);
+%Make sure the Base decomposes the samples as part of the signal, not as
+%the signal itself!
+for t=1:cs2 %border values possibly added to the clipped values - SOLVED
+    if(data(1,t)<MaxS && data(1,t)>MinS)
+        samples=[samples data(1,t)];
+    elseif(((t==1 && data(1,t+1)-data(1,t)==0)  || (t==cs2 && data(1,t)-data(1,t-1)==0))... 
+            || ((t~=1 && t~=cs2) && data(1,t-1)-data(1,t+1))==0)
+        M=[M t];
+%         samples=[samples 0];
+        if data(1,t)==MaxS
+            Mp=[Mp t];
+        else
+            Mn=[Mn t];
+        end
+    else
+        samples=[samples data(1,t)];
+    end
+end
+samples=samples';
+%Remove all rows in the DCT base, according to the sampling matrix!
+% A=DCTBase(N,N,-1);
+A=dctmtx(N)';
+B=A;
+A(M,:)=[]; 
+
+if length(A)==0
+    disp('Clipping length exceeds frame length')
+    return;
+end
+
+%Extra constraints
+Mpos=zeros(N,1);
+Mpos(Mp,:)=1;
+Mneg=zeros(N,1);
+Mneg(Mn,:)=1;
+MclA=diag(Mneg-Mpos)*B;
+eps=0.9;
+offSet=max(abs(samples))*eps;
+
+
+
+
+if regularization==[]
+    regularization=0.01;
+end
+if methodChoice == []
+    methodChoice=3;
+end
+%Solve the constrained L1 optimization (with lambda regularization)
+switch methodChoice
+    case 1
+        x=SolveOMP(A,samples,N,50); %--FAST FAVORITE SO FAR
+    case 2
+        x=OMPDeclip(A,samples,N,MclA,50); %--FAST FAVORITE SO FAR
+    case 3 
+        x=SolveBP(A,samples,N,50,regularization,1e-4); %Investigate parameter impact
+%         options = optimset('Algorithm','interior-point','Display','on');
+%         [x, fval]=fmincon(@L1Norm,offSet*ones(N,1),MclA,offSet*ones(length(MclA),1),[],[],[],[],@L2Norm,options);
+    case 4
+        x=IRL1(A,samples,N,50,0,1e-4); %Development in progress
+%           x=Threshold_ISD_1D(A,samples);
+    case 5
+%         x=SolveLasso(A,samples,N); %--VERY SLOW, NOT THAT ACCURATE
+          options = struct('Verbose',0);
+          [x,niter,residuals,outputData,opts] =NESTA(A,[],samples,0.01,1e-4,options);
+end
+
+r=idct(x)';
+data(1,M)=r(1,M);
+r=data;
+
+% for g=Mp
+%     temp=r(1,g)
+%     if r(1,g)<MaxS
+%         r(1,g)=MaxS;
+%     end
+% end
+% for h=Mn
+%     if r(1,h)>MinS
+%         r(1,h)=MinS;
+%     end
+% end
+
+% subplot(5,1,1);plot(data);
+% title('Clipped signal')
+% axis([0 N (min(data)-1) (max(data)+1)])
+% subplot(5,1,2);plot(r(1:N,1));
+% title('Reconstructed signal')
+% axis([0 N (min(r)-1) (max(r)+1)])
+% % subplot(5,1,3);plot(orig)
+% % title('Original signal')
+% % axis([0 N (min(orig)-1) (max(orig)+1)])
+% subplot(5,1,4);plot(samples)
+% title('Samples')
+% axis([0 N (min(samples)-1) (max(samples)+1)])
+% subplot(5,1,5);plot(abs(x))
+% title('Spectral representation of reconstruction')
+% axis([0 N (min(abs(x))-1) (max(abs(x))+1)])
+end
+
---
+++ MATLAB/maskingThreshold.m
@@ -0,0 +1,477 @@
+function [ threshold ] = maskingThreshold( input , showplots)
+%calculates masking threshold of input audio signal
+%   Steven De Hertogh
+% 
+
+
+if (nargin < 2)
+    showplots = 0;
+end
+% Reading inputsignal
+%[in, Fs, nbits] = wavread(input);
+
+if( input == 1 )
+    Tq = [];
+    for f = 1:22000
+         Tq(f) = 3.64*(f/1000)^(-0.8) - 65*exp(-0.6*(f/1000-3.3)^2) + 10^(-3) * (f/1000)^4;
+    end
+
+    threshold=(downsample(Tq, 86));
+else
+
+    in = input';
+
+    %FFT length
+    N = 512;
+    nbits = 16;
+
+    % Normalization
+    sig = [];
+    for i = 1:length(in)
+    sig(i) = in(i,1)/(N*2^(nbits-1));
+    end
+    %sig = sig(10000:50000);
+
+    if (showplots == 1)
+        figure();
+        subplot(3,1,1);plot(in(:,1));
+        %subplot(3,1,2);plot(in(:,2));
+        subplot(3,1,3);plot(sig);
+    end
+    
+    % PSD
+    P=[];
+    z=zeros(1,N/2);
+    PN = 90;
+    for k = 1:N/2
+        w = [];
+       for n = 1:N
+           w(n) = 1/2*(1-cos(2*pi*n/N));
+           z(k) = z(k) + w(n)*in(n,1)*exp(-j*2*pi*k*n/N);
+       end
+       P(k) = PN + 10*log10(abs(z(k))^2);
+    end
+
+    if (showplots == 1)
+        figure();
+        plot(P);
+    end
+
+    % Tonal set
+    St=zeros(1,N/2);
+
+    for k = 3:62 %(0,17-5,5 kHz)
+        if (P(k) > P(k+1)) && (P(k) > P(k-1)) && (P(k) > (P(k+2)+7)) && (P(k) > (P(k-2)+7))
+             St(k) = P(k);
+        end
+    end
+    for k = 63:126 %(5,5-11 kHz)
+        if (P(k) > P(k+1)) && (P(k) > P(k-1)) && (P(k) > (P(k+2)+7)) && (P(k) > (P(k-2)+7)) && (P(k) > (P(k+3)+7)) && (P(k) > (P(k-3)+7))
+             St(k) = P(k);
+        end
+    end
+    for k = 127:250 %(11-20 kHz) 
+        if (P(k) > P(k+1)) && (P(k) > P(k-1)) && (P(k) > (P(k+2)+7)) && (P(k) > (P(k-2)+7)) && (P(k) > (P(k+6)+7)) && (P(k) > (P(k-6)+7))
+             St(k) = P(k);
+        end
+    end
+
+    if (showplots == 1)
+        i = 1;
+        a= [];
+        b= [];
+        for k = 1:256
+           if St(k) ~= 0
+               a(i) = St(k);
+               b(i) = k;
+               i = i+1;
+           end
+        end
+        hold on
+        plot(P);
+        plot(b,a, 'o');
+        %plot(St, 'o');
+        hold off
+    end
+
+    % Tonal and Noise Maskers
+    Ptm = zeros(1,N/2);
+
+    for k = 2:(N/2-1)
+       w = 0;
+       if St(k) ~= 0
+        for c = -1:1
+           w = w + 10^(0.1*St(k+c));
+        end
+       end
+       Ptm(k) = 10*log10(w);
+    end
+
+    Pn = zeros(1,N/2);
+    for k = 3:62 %(0,17-5,5 kHz)
+        if (P(k) ~= St(k)) && (P(k) ~= St(k+1)) && (P(k) ~= St(k-1)) && (P(k) ~= St(k+2)) && (P(k) ~= St(k-2))
+             Pn(k) = P(k);
+        end
+    end
+    for k = 63:126 %(5,5-11 kHz)
+        if (P(k) ~= St(k)) && (P(k) ~= St(k+1)) && (P(k) ~= St(k-1)) && (P(k) ~= (St(k+2)+7)) && (P(k) ~= St(k-2)) && (P(k) ~= St(k+3)) && (P(k) ~= St(k-3))
+             Pn(k) = P(k);
+        end
+    end
+    for k = 127:250 %(11-20 kHz) 
+        if (P(k) ~= St(k)) && (P(k) ~= St(k+1)) && (P(k) ~= St(k-1)) && (P(k) ~= (St(k+2)+7)) && (P(k) ~= St(k-2)) && (P(k) ~= St(k+6)) && (P(k) ~= St(k-6))
+             Pn(k) = P(k);
+        end
+    end
+
+    Pnm = zeros(1,N/2);
+    i = 1;
+    k = 1;
+    while k < N/2
+        kstart = k;
+        while ((ceil(FreqToBark(k*86)) == i) && (k < (N/2)))
+            k = k+1;
+        end
+        kstop = k;
+        i = i+1;
+        kstreep = 1;
+        for z = kstart:kstop
+           kstreep = kstreep * z;
+        end
+        kstreep = kstreep^(1/(kstop-kstart+1));
+        kstreep = floor(kstreep);
+        w = 0;
+        nottonal = 1;
+        for n = kstart:kstop
+            if Pn(n) == 0
+                nottonal = 0;
+            else
+                w = nottonal*(w + 10^(0.1*Pn(n)));
+            end
+        end
+        Pnm(kstreep) = 10*log10(w);
+    end
+
+    if (showplots == 1)
+        i = 1;
+        n = 1;
+        a= [];
+        b= [];
+        c= [];
+        d= [];
+        for k = 1:256
+           if Ptm(k) ~= 0
+               a(i) = Ptm(k);
+               b(i) = k;
+               i = i+1;
+           end
+           if Pnm(k) ~= 0
+               c(n) = Pnm(k);
+               d(n) = k;
+               n = n+1;
+           end
+
+        end
+        figure();
+        hold on
+        plot(P);
+        plot(b,a, 'o');
+        plot(d,c, 'x');
+        hold off
+    end
+
+
+
+
+    % Absolute threshold
+    Tq = [];
+    for f = 1:22000
+        Tq(f) = 3.64*(f/1000)^(-0.8) - 65*exp(-0.6*(f/1000-3.3)^2) + 10^(-3) * (f/1000)^4;
+    end
+
+    tq=(downsample(Tq, 86));
+
+    if (showplots ==1)
+        figure();
+        hold on
+        plot(P);
+        plot(b,a, 'o');
+        plot(d,c, 'x');
+        semilogx(tq);
+        hold off
+    end
+
+    % Decimation of maskers
+    for k = 1:N/2
+        if Ptm(k) < tq(k)
+            Ptm(k) = 0;
+        end
+        if Pnm(k) < tq(k)
+            Pnm(k) = 0;
+        end
+    end
+
+
+    if (showplots == 1)
+        i = 1;
+        n = 1;
+        a= [];
+        b= [];
+        c= [];
+        d= [];
+        for k = 1:256
+           if Ptm(k) ~= 0
+               a(i) = Ptm(k);
+               b(i) = k;
+               i = i+1;
+           end
+           if Pnm(k) ~= 0
+               c(n) = Pnm(k);
+               d(n) = k;
+               n = n+1;
+           end
+        end
+        figure();
+        hold on
+        plot(P);
+        plot(b,a, 'o');
+        plot(d,c, 'x');
+        semilogx(tq);
+        hold off
+    end
+
+
+    % Sliding 0.5 bark window
+    %tone
+    ks = [];
+    b = [];
+    i=1;
+    for k = 1:N/2
+       if Ptm(k)>0
+           ks(i)=k;
+           b(i) =  FreqToBark(k*86);
+           i = i+1;
+       end    
+    end
+
+
+    max = 0;
+    maxki = 1;
+    for i = 1:length(ks)
+         if (b(i) - b(maxki)) < 0.5
+             if (Ptm(ks(i)) > max)
+                 max = Ptm(ks(i));
+                 maxki = i;
+             else
+                 Ptm(ks(i)) = 0;
+             end
+         else
+         max = 0;
+         maxki = i;
+         end
+    end
+
+    %noise
+    ks = [];
+    b = [];
+    i=1;
+    for k = 1:N/2
+       if Pnm(k)>0
+           ks(i)=k;
+           b(i) =  FreqToBark(k*86);
+           i = i+1;
+       end    
+    end
+
+
+    max = 0;
+    maxki = 1;
+    for i = 1:length(ks)
+         if (b(i) - b(maxki)) < 0.5
+             if (Pnm(ks(i)) > max)
+                 max = Pnm(ks(i));
+                 maxki = i;
+             else
+                 Pnm(ks(i)) = 0;
+             end
+         else
+         max = 0;
+         maxki = i;
+         end
+    end
+
+
+
+
+    if (showplots == 1)
+        i = 1;
+        n = 1;
+        a= [];
+        b= [];
+        c= [];
+        d= [];
+        for k = 1:256
+           if Ptm(k) ~= 0
+               a(i) = Ptm(k);
+               b(i) = k;
+               i = i+1;
+           end
+           if Pnm(k) ~= 0
+               c(n) = Pnm(k);
+               d(n) = k;
+               n = n+1;
+           end
+        end
+        figure();
+        hold on
+        plot(P);
+        plot(b,a, 'o');
+        plot(d,c, 'x');
+        semilogx(tq);
+        hold off
+    end
+
+
+    %Decimation and reorganization
+    Ptm2 = zeros(1,length(Ptm));
+    Pnm2 = zeros(1,length(Pnm));
+    for k = 1:length(Ptm)
+        if  (k >= 1) && (k <= 48)
+            i = k;
+            Ptm2(i) = Ptm(k);
+            Pnm2(i) = Pnm(k);
+        elseif (k >= 49) && (k <= 96)
+            i = k+mod(k,2);
+            Ptm2(i) = Ptm(k);
+            Ptm(k) = 0;
+            Pnm2(i) = Pnm(k);
+            Pnm(k) = 0;
+        elseif (k >= 97) && (k <= 232)
+            i = k + 3 - mod((k-1),4);
+            Ptm2(i) = Ptm(k);
+            Ptm(k) = 0;
+            Pnm2(i) = Pnm(k);
+            Pnm(k) = 0;
+        end
+    end
+
+    %Niet enkel voor plots! ook voor global threshold!
+    i = 1;
+    n = 1;
+    a= [];
+    b= [];
+    c= [];
+    d= [];
+    for k = 1:256
+       if Ptm2(k) ~= 0
+           a(i) = Ptm2(k);
+           b(i) = k;
+           i = i+1;
+       end
+       if Pnm2(k) ~= 0
+           c(n) = Pnm2(k);
+           d(n) = k;
+           n = n+1;
+       end
+    end
+
+    if (showplots == 1)
+        figure();
+        hold on
+        plot(P);
+        plot(b,a, 'o');
+        plot(d,c, 'x');
+        semilogx(tq);
+        hold off
+    end
+
+    %Individual masking thresholds
+    SF = [];
+    SFn = [];
+    Ttm = [];
+    Tnm = [];
+    for i = 1:N/2
+        for n = 1:N/2
+            delt = FreqToBark(i*86) - FreqToBark(n*86);
+            if (delt >= -3) && (delt < -1)
+                SF(i,n) = 17*delt - 0.4*Ptm2(n) + 11;
+                SFn(i,n) = 17*delt - 0.4*Pnm2(n) + 11;
+            elseif (delt >= -1) && (delt < 0)
+                SF(i,n) = (0.4*Ptm2(n)+6)*delt;
+                SFn(i,n) = (0.4*Pnm2(n)+6)*delt;
+            elseif (delt >= 0) && (delt < 1)
+                SF(i,n) = -17*delt;
+                SFn(i,n) = -17*delt;
+            elseif (delt >= 1) && (delt < 8)
+                SF(i,n) = (0.15*Ptm2(n) - 17)*delt - 0.15*Ptm2(n);
+                SFn(i,n) = (0.15*Pnm2(n) - 17)*delt - 0.15*Pnm2(n);
+            else
+                SF(i,n) = -150;
+                SFn(i,n) = -150;
+            end
+            Ttm(i,n) = Ptm2(n) - 0.275*FreqToBark(n*86) + SF(i,n) - 6.025;
+            Tnm(i,n) = Pnm2(n) - 0.175*FreqToBark(n*86) + SF(i,n) - 2.025;
+        end
+    end
+    %size(Ttm)
+    %Ttm(50,:)
+
+    if (showplots == 1)
+        figure();
+        hold on;
+        %plot(Ttm);
+
+        plot(Ttm(:,50));
+        plot(Ttm(:,20));
+        plot(Ttm(:,100));
+        plot(Ttm(:,150));
+        plot(Ttm(:,200));
+
+        plot(b,a, 'o');
+        %plot(Ttm(200,:));
+        hold off;
+
+        figure();
+        hold on;
+        %plot(Ttm);
+        plot(Tnm(:,50));
+        plot(Tnm(:,10));
+        plot(Tnm(:,20));
+        plot(Tnm(:,30));
+        plot(Tnm(:,100));
+        plot(Tnm(:,150));
+        plot(d,c, 'x');
+        %plot(Ttm(200,:));
+        hold off;
+    end
+
+
+    %Global masking thresholds
+
+    Tg = [];
+    for i = 1:N/2
+        SumTtm = 0;
+        for l = 1:length(Ttm)
+           SumTtm = SumTtm + 10^(0.1*Ttm(i,l)); 
+        end
+        SumTnm = 0;
+        for m = 1:length(Tnm)
+           SumTnm = SumTnm + 10^(0.1*Tnm(i,m)); 
+        end
+        Tg(i) = 10*log10(10^(0.1*Tq(i))+SumTtm+SumTnm);
+    end
+
+    threshold = Tg;
+
+    if (showplots == 1)
+        figure();
+        hold on
+        plot(Tg);
+        semilogx(tq);
+        hold off
+    end
+   % figure(1);
+    %plot(Tg(10:length(Tg)));
+end
+end
+
